7977046c03f4741ee0500d1706318a638c15bf91
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-01-28 09:46:28 -0600

    Development changing

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  has_many :comments
-  has_many :posts, :through => :comments
-
-  validates :name, presence: true
-  validates :email, presence: true
-end
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "posts#index"
-  resources :users
-  resources :posts do
-    resources :comments
-  end
-end
